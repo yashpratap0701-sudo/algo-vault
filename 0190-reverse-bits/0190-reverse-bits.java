ca4a45a294ab74a2bb81d7836b43d1916f5bca44
yashpratap0701-sudo <yashpratap0701@gmail.com>
2026-08-11 16:57:09 +0530

    Time: 0 ms (100%), Space: 42.4 MB (52.7%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,21 +1,11 @@
 class Solution {
-   public static int reverseBits(int n) {
-
-        int[] num1 = new int[32];
-
-        
-        for (int i = 0; i < 32; i++) {
-            num1[i] = n & 1;
-            n = n >>> 1;
+   public int reverseBits(int n) {
+        int result=0;
+        for(int i=0;i<32;i++){
+            result= result<<1;
+            result= result|(n&1);
+            n=n>>>1;
         }
-
-       
-        int num2 = 0;
-
-        for (int i = 0; i < 32; i++) {
-            num2 = (num2 << 1) | num1[i];
-        }
-
-        return num2;
+        return result;
     }
 }
