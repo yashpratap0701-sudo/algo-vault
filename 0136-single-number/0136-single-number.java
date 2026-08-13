b6f7f077d3d873468d2f9c4b6b053327fa153659
yashpratap0701-sudo <yashpratap0701@gmail.com>
2026-08-13 08:49:32 +0530

    Time: 9 ms (28.25%), Space: 47.1 MB (28.24%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,24 @@
 class Solution {
     public int singleNumber(int[] arr) {
 
-        int unique = 0;
+        int ans = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            unique = unique ^ arr[i];
+        for (int i = 0; i < 32; i++) {
+
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+
+                if ((arr[j] & (1 << i)) != 0) {
+                    count++;
+                }
+            }
+
+            if (count % 2 != 0) {
+                ans = ans | (1 << i);
+            }
         }
 
-        return unique;
+        return ans;
     }
 }
