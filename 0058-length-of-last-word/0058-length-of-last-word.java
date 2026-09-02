f48e1f7d23dfbae4dc50b5b6fe1f000b3078e966
yashpratap0701-sudo <yashpratap0701@gmail.com>
2026-09-02 14:52:21 +0530

    Time: 0 ms (100%), Space: 43 MB (69.03%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+    int count = 0;
+
+    for (int t = s.length() - 1; t >= 0; t--) {
+
+        if (s.charAt(t) != ' ') {
+            count++;
+        } 
+        else if (count > 0) {
+            break;
+        }
+    }
+
+    return count;
+}
+ }
